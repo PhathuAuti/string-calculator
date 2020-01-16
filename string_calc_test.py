5861d7cd856dede931409640e6b70bee840b9a8f
PhathuAuti <phathuauti@gmail.com>
2020-01-16 15:21:38 +0200

    Added Pytest

---
+++ string_calc_test.py
@@ -0,0 +1,38 @@
+import  pytest
+from string_calculator import add
+
+#  testing empty string returns 0
+def test_add_empty_string():
+    assert add("") == 0
+
+#  testing addition of positive numbers of any string length
+def test_add_positive_integers():
+    assert add("1") == 1
+    assert add("1,2") == 3
+    assert add("1,3,5,9") == 18
+
+# testing to handle the new line characters
+def test_add_with_new_lines():
+    assert add("1\n2,3") == 6
+
+# testing to support delimiters of any shapes and sizes
+def test_add_with_delimiters():
+    assert add("//;\n1;2") == 3
+    # assert add("//[]\n12***3") == 6
+    # assert add("//[][%]\n12%3") == 6
+    # assert add("//[][%]//[]\n12***3\n12%3") == 12
+
+#  testing negatives not allowed
+def test_add_negative_integers():
+    with pytest.raises(Exception) as error:
+        assert add("-1,-2,3,4")
+    assert str(error.value) == 'ERROR: negatives not allowed -1,-2'
+
+# testing numbers bigger than a 1000
+def test_add_bigger_than_thousand():
+    assert add("//;\n1000,1;2") == 3
+
+
+
+
+    
---
+++ string_calculator.py
@@ -0,0 +1,42 @@
+import re
+
+def add(string):
+    # Extract numbers (both positive and negative) into a string numbers list x
+    integers = re.findall(r"-?\d+", string)
+    total_sum = 0
+    negatives = extractNegatives(integers)
+    if len(string) == 0:
+        return 0
+
+    if len(string) == 1:
+        try:
+            return(int(string))
+        except:
+            return("ERROR. Only accepts numbers.")
+
+    if len(string) >= 3:
+        if len(negatives) > 0:
+            error_msg = 'ERROR: negatives not allowed '
+            for number in negatives:
+                # first check if it is the last number so that you don't add a comma
+                if negatives.index(number) != len(negatives) - 1:
+                    error_msg += str(number) + ','
+                else:
+                    error_msg += str(number)
+            raise Exception (error_msg)
+        else:
+            for number in integers:
+                if int(number) < 1000:
+                    total_sum += int(number)
+    return total_sum
+
+# helper function to return a list of negative numbers
+def extractNegatives(numbers_list):
+    negatives = []
+    for number in numbers_list:
+        if int(number) < 0:
+            negatives.append(int(number))
+    return negatives
+
+# print(add("//;\n1;2"))
+# print(add("-1,-2,3,4"))
